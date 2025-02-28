020f7888975e2ff9ab1f839eba852dfccf660ae8
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-28 19:30:31 +0800

    Swap the cut image

--- src/components/SliderPuzzle.js
+++ src/components/SliderPuzzle.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import moment from 'moment'
 import './SliderPuzzle.css'
 
@@ -14,23 +14,45 @@ function SliderPuzzle({ puzzleId = 'cut-pink' }) {
     'image_part_008.jpg',
     'image_part_009.jpg',
   ]
-  const [shuffledPuzzleArray, setShuffledPuzzleArray] = useState([...correctPuzzleArray].sort(
-      () => Math.random() - 0.5
-    ))
+  const [shuffledPuzzleArray, setShuffledPuzzleArray] = useState(
+    [...correctPuzzleArray].sort(() => Math.random() - 0.5)
+  )
+  // const [timer, setTimer] = useState(null)
+  const [indexesToSwap, setIndexesToSwap] = useState([])
+
+  function swap(index) {
+    // if (!timer) {
+    //   return;
+    // }
+    let its = []
+    let spa = []
+    if (indexesToSwap.length < 2) {
+      its = [...indexesToSwap, index]
+    }
+    if (its.length === 2) {
+      const [index1, index2] = its
+      spa = [...shuffledPuzzleArray]
+      const tmp = spa[index1]
+      spa[index1] = spa[index2]
+      spa[index2] = tmp
+      setShuffledPuzzleArray(() => [...spa])
+      setIndexesToSwap(() => [])
+    } else {
+      setIndexesToSwap(() => [...its])
+    }
+  }
 
   return (
     <div>
       <h1>Swap the Images to Win</h1>
-      <div className="row">
-        {
-          shuffledPuzzleArray.map((s, index) => {
-            return (
-              <div className="column" key={index}>
-                <img src={`assets/${puzzleId}/${s}`} />
-              </div>
-            )
-          })
-        }
+      <div className='row'>
+        {shuffledPuzzleArray.map((s, index) => {
+          return (
+            <div className='column' key={index} onClick={() => swap(index)}>
+              <img src={`assets/${puzzleId}/${s}`} />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
