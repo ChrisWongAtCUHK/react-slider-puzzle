6f21f60731641c22ed5ddc1b1f199902e45fa69a
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-28 19:48:27 +0800

    Show elapsed time

--- src/components/SliderPuzzle.js
+++ src/components/SliderPuzzle.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import moment from 'moment'
 import './SliderPuzzle.css'
 
@@ -17,8 +17,13 @@ function SliderPuzzle({ puzzleId = 'cut-pink' }) {
   const [shuffledPuzzleArray, setShuffledPuzzleArray] = useState(
     [...correctPuzzleArray].sort(() => Math.random() - 0.5)
   )
-  // const [timer, setTimer] = useState(null)
+  const [timer, setTimer] = useState(null)
   const [indexesToSwap, setIndexesToSwap] = useState([])
+
+  const [startDateTime, setStartDateTime] = useState(new Date())
+  const [currentDateTime, setCurrentDateTime] = useState(new Date())
+  const [elapsedDiff, setElapsedDiff] = useState(moment(new Date))
+  const [elapsedTime, setElapsedTime] = useState('')
 
   function swap(index) {
     // if (!timer) {
@@ -42,9 +47,36 @@ function SliderPuzzle({ puzzleId = 'cut-pink' }) {
     }
   }
 
+  function resetTime() {
+    setStartDateTime(new Date())
+    setCurrentDateTime(new Date())
+  }
+
+  function start() {
+    resetTime();
+    setShuffledPuzzleArray(() => [...correctPuzzleArray].sort(
+      () => Math.random() - 0.5
+    ))
+    setIndexesToSwap([])
+    const timer = setInterval(() => {
+      const currentDT = moment(new Date());
+      const startDT = moment(startDateTime);
+      const diff = currentDT.diff(startDT);
+      const elapsedT = moment.utc(diff).format("HH:mm:ss")
+      setElapsedTime(() => elapsedT)
+      // if (this.isWinning) {
+      //   this.recordSpeedRecords();
+      //   this.stop();
+      // }
+    }, 1000);
+    setTimer(() => timer)
+  }
+
   return (
     <div>
       <h1>Swap the Images to Win</h1>
+      <button onClick={start} id="start-button">Start Game</button>
+      <p>Elapsed Time: { elapsedTime }</p>
       <div className='row'>
         {shuffledPuzzleArray.map((s, index) => {
           return (
